b038e374c26e0ea28ed926afd43effbc31925664
Trevor Johnson <johnson.trevor.kent@gmail.com>
2023-09-26 11:43:00 -0600

    Added operate function

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+let num1 = 0;
+let num2 = 0;
+let operator = undefined;
+
 function add(a, b) {
     return (a + b);
 };
@@ -11,5 +15,21 @@ function multiply(a, b) {
 };
 
 function divide(a, b) {
-    return (a + b);
+    return (a / b);
+};
+
+function operate(op, a, b){
+    //1 is +, 2 is -, 3 is *, 4 is /
+    if (op == 1){
+        return add(a, b);
+    }
+    if (op == 2){
+        return subtract(a, b);
+    }
+    if (op == 3){
+        return multiply(a, b);
+    }
+    if (op == 4){
+        return divide(a, b);
+    }
 };
